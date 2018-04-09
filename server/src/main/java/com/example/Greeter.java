452490318c0399acafc97949e3471bf6ac772450
Teuber <michael@teuber.dk>
2018-04-09 14:41:01 +0200

    finalfix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,15 +1,3 @@
-/**
- * Domain classes used to produce .....
- * <p>
- * These classes contain the ......
- * </p>
- *
- * @since 1.0
- * @author somebody
- * @version 1.0
- */
-
-
 package com.example;
 
 
