903411c5b85132d89feb737e9d3b66212dd4f9c7
Teuber <michael@teuber.dk>
2018-04-09 14:33:36 +0200

    more fixes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,4 +1,18 @@
+/**
+ * Domain classes used to produce .....
+ * <p>
+ * These classes contain the ......
+ * </p>
+ *
+ * @since 1.0
+ * @author somebody
+ * @version 1.0
+ */
+
+
 package com.example;
+
+
 
 /**
  * This is a class.
@@ -16,7 +30,7 @@ public class Greeter {
    * @param //name of someone
    * @return personalised message
    */
-  public final String greet(String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
